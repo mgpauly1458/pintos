f3c9a0930319870379c6434ab53e6448fbc5fa8e
maxwell <maxwell@EE-468-Ubuntu.myguest.virtualbox.org>
2023-10-11 12:15:24 -1000

    skeleton for write syscall made

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -12,9 +12,27 @@ syscall_init (void)
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
 }
 
+void *extract_arg(struct intr_frame *f, int offset) {
+	return f->esp + offset;
+}
+
 static void
-syscall_handler (struct intr_frame *f UNUSED) 
+syscall_handler (struct intr_frame *f) 
 {
+  int syscall_number = *(int*)f->esp;
+
+  switch(syscall_number) {
+	case 9: sys_write(f); break;
+	default: ;
+  }
   printf ("system call!\n");
   thread_exit ();
+}
+
+void sys_write(struct intr_frame *f) {
+	int fd = *(int*)extract_arg(f, 4);
+	void *buf = *(void **)extract_arg(f, 8);
+	unsigned length = *(unsigned *)extract_arg(f, 12);
+	printf("sys_write\n");
+	printf("fd=%d buf=%p length=%d\n", fd, buf, length);
 }
